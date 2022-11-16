173b3b2701366e21a44b6ffcc866c07240b42055
violayad98 <viola.yad98@gmail.com>
2022-11-16 17:51:06 +0200

    change default logic

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php 
-header('Location: /views/login.php');
+<?php
+session_start();
+
+if (empty($_SESSION['user'])) {
+    header('Location: /views/login.php');
+} else {
+    header('Location: /vendor/film_list.php');
+}
 
 ?>
